53b4a4a2e227d345ce6b78852361edb4e32820d5
pjanapy <piotr.janaszkiewicz@gmail.com>
2025-06-20 13:33:59 +0200

    Finish scraping products

--- rossmann/scripts/scraper.py
+++ rossmann/scripts/scraper.py
@@ -1,43 +1,51 @@
 from bs4 import BeautifulSoup
 import requests, pickle, re
-
-product_list_pickle_filename = 'product_list.pickle'
-products_data_pickle_filename = 'products_data.pickle'
+import pandas as pd
 
 #
-# Ustawienie parametrów
+# Parametry ogólne
 #
 base_url = 'https://www.rossmann.pl'
-# Strony kategorii
+product_list_pickle_filename = './data/male_product_list.pkl'
+products_data_pickle_filename = './data/male_products_data.pkl'
+
+#
+# Parametry przeszukiwania listy stron
+#
 scrape_product_list = False
-get_product_list_from_file = False
+get_product_list_from_file = True
 save_product_list_to_file = True
 product_pages = 75
 category_url = '/kategoria/mezczyzna,13224'
 page_prefix = '?Page='
 
-# Strony produktów
-scrape_products_pages = False
-get_products_data_from_file = False
+#
+# Parametry przeszukiwania stron produktów
+#
+scrape_products_pages = True
 save_products_data_to_file = True
 start = 0
-stop = 4000
+stop = 2000
 
-# Dane wyjściowe
+#
+# Nazwy kolumn w danych wyjściowych
+#
 columns = ['product_id',
            'name',
            'caption',
            'cat_1',
            'cat_2',
            'cat_3',
+           'cat_4',
+           'cat_5',
            'url',
            'capacity',
-           'current_price',
-           'regular_price',
-           'lowest_price_last_30_days',
-           'price_per_unit',
+           'current_price_pln',
+           'regular_price_pln',
+           'lowest_price_last_30_days_pln',
+           'price_per_unit_pln',
            'unit',
-           'badges',
+           'mega_badge',
            'ingredients',
            'rating',
            'reviews',     
@@ -45,11 +53,17 @@ columns = ['product_id',
 
 products_url = []
 
-def scrape_products_urls():
-    print('Pobieranie listy produktów ze strony.')
+def scrape_products_urls(base_url, category_url, page_prefix, num_of_pages):
+    '''
+    Funkcja pobierająca dane ze stron określonej kategorii.
+
+    Returns:
+        set: zbiór linków produktów określonej kategorii.
+    '''
     product_list = []
 
-    for page_number in range(1, product_pages + 1):
+    for page_number in range(1, num_of_pages + 1):
+        print(f"Pobieranie linków ze strony {page_number} kategorii.")
         response = requests.get(base_url + category_url + page_prefix + str(page_number))
         
         if response.status_code == 200:
@@ -60,10 +74,18 @@ def scrape_products_urls():
                 href = link.get("href")
                 if type(href) == str and href.startswith('/Produkt/'):
                     product_list.append(href)
-            
-    return product_list
 
-def scrape_product_page(page_url):
+    link_set = set(product_list)
+    print(f"Zwracam {len(link_set)} linków produktów.")
+    return link_set
+
+def scrape_product_page(base_url, page_url):
+    '''
+    Funkcja pobierająca dane ze strony określonego na podstawie linku produktu.
+
+    Returns:
+        list: lista z 19 polami danych produktu
+    '''
     page_data = []
     response = requests.get(base_url + page_url)
     if response.status_code == 200:
@@ -90,6 +112,18 @@ def scrape_product_page(page_url):
         # cat_3
         a = list(soup.find(attrs={"class": "styles-module_ol--FwihY"}).children)[4].text
         page_data.append(a)
+        # cat_4
+        if len(list(soup.find(attrs={"class": "styles-module_ol--FwihY"}).children)) > 5:
+            a = list(soup.find(attrs={"class": "styles-module_ol--FwihY"}).children)[5].text
+        else:
+            a = ''
+        page_data.append(a)
+        # cat_5
+        if len(list(soup.find(attrs={"class": "styles-module_ol--FwihY"}).children)) > 6:
+            a = list(soup.find(attrs={"class": "styles-module_ol--FwihY"}).children)[6].text
+        else:
+            a = ''
+        page_data.append(a)
         # url
         page_data.append(base_url + page_url)
         # capacity
@@ -97,30 +131,34 @@ def scrape_product_page(page_url):
         page_data.append(a)
         # current_price
         a = soup.find(attrs={"data-testid": "product-price"}).text.strip()
+        a = float(a.replace("zł", "").replace(" ", "").replace(",", "."))
         page_data.append(a)
         # regular_price
         if soup.find(attrs={"data-testid": "basic-price"}) is not None:
             a = soup.find(attrs={"data-testid": "basic-price"}).text.strip()
+            a = float(a.replace("zł", "").replace(" ", "").replace(",", "."))
         else:
             a = ''
         page_data.append(a)
         # lowest_price_last_30_days
         if soup.find(attrs={"data-testid": "lowest-price-in-30-days"}) is not None:
             a = soup.find(attrs={"data-testid": "lowest-price-in-30-days"}).text.strip()
+            a = float(a.replace("zł", "").replace(" ", "").replace(",", "."))
         else:
             a = ''
         page_data.append(a)
         # price_per_unit
         a = soup.find(attrs={"data-testid": "product-price-per-unit"}).text.split(" = ")[1]
+        a = float(a.replace("zł", "").replace(" ", "").replace(",", "."))
         page_data.append(a)
         # unit 
         a = soup.find(attrs={"data-testid": "product-price-per-unit"}).text.split(" = ")[0]
         page_data.append(a)
-        # badges
+        # mega_badge
         if soup.find(attrs={"data-testid": "mega-badge"}) is not None:
-            a = soup.find(attrs={"data-testid": "mega-badge"}).text
+            a = 1
         else:
-            a = ''
+            a = 0
         page_data.append(a)
         # ingredients
         if list(soup.find(attrs={"class": "styles-module_productDescription--Y3jcw"}).children)[1].span.text == 'Składniki':
@@ -143,28 +181,35 @@ def scrape_product_page(page_url):
         
         return page_data
 
-
+#
+# Instrukcje wykonania pobierania danych ze stron kategorii.
+#
 if scrape_product_list:
-    products_url = scrape_products_urls()
+    products_url = scrape_products_urls(base_url,
+                                        category_url,
+                                        page_prefix,
+                                        product_pages)
     if save_product_list_to_file:
         with open(product_list_pickle_filename, 'wb') as file:
+            print('Zapisuję listę poduktów.')
             pickle.dump(products_url, file)
 elif get_product_list_from_file:
     print('Pobieranie listy produktów z pliku.')
     with open(product_list_pickle_filename, 'rb') as file:
         products_url = pickle.load(file)
 
+#
+# Instrukcje wykonania pobierania danych ze stron produktów.
+#
 if scrape_products_pages:
     products_data = []
     for i, page in enumerate(products_url):
         if i >= start and i <= stop:
-            print('Scraping page no. ' + str(i))
-            products_data.append(scrape_product_page(page))
-    products_data = [columns] + products_data
+            print('Pobieranie danych ze strony ' + str(i))
+            products_data.append(scrape_product_page(base_url, page))
     if save_products_data_to_file:
+        products_data = [p for p in products_data if p is not None]
+        df = pd.DataFrame(products_data, columns = columns)
         with open(products_data_pickle_filename, 'wb') as file:
-            pickle.dump(products_data, file)
-elif get_products_data_from_file:
-    print('Pobieranie danych produktów z pliku.')
-    with open(products_data_pickle_filename, 'rb') as file:
-        products_data = pickle.load(file)
+            print('Zapisuję dane poduktów.')
+            pickle.dump(df, file)
